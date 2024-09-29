e1fcb7333f8cfd74d56f5e0a05d2cfbc677fb083
soeyadanaraung <soeyadanaraung@my.jcu.edu.au>
2024-09-29 16:03:31 +0800

    Add password check program

--- password_stars.py
+++ password_stars.py
@@ -1,8 +1,8 @@
 MINIMUM_LENGTH = 5
-password = input("Enter your password: ")
+password = input("Enter a password: ")
 while len(password) < MINIMUM_LENGTH:
     print("The password doesn't meet the minimum length")
-    password = input("Enter your password: ")
+    password = input("Enter a password: ")
 print("*" * len(password))
 
 
